85146a6d7ed6082421b96c26cd014eecacacf446
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-13 16:51:05 -0300

    diseño basico nav superior

--- client/src/components/NavegacionLibroAsistencias.jsx
+++ client/src/components/NavegacionLibroAsistencias.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavegacionLibroAsistencias = (props) => {
     return (
         <div className='w-screen'>
-            <h1 className='bg-secondary-middle text-white p-8'>Libro de asistencia</h1>
+            <h1 className='bg-secondary-middle text-white p-8 h-44 text-center pt-16 text-4xl'>LIBRO DE ASISTENCIA</h1>
             {props.children}
         </div>
     );
