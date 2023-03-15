6937881b59b092b73bef7dd61ab6661eba934f88
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-15 12:39:23 -0300

    deseño basico de formulario de nuevo trabajador

--- client/src/views/NuevoTrabajador.jsx
+++ client/src/views/NuevoTrabajador.jsx
@@ -1,9 +1,176 @@
 import React from 'react';
+import {Formik, Field,Form} from 'formik';
+import * as Yup from 'yup';
 
 const NuevoTrabajador = () => {
+    //agregar alert cuando se agrega un nuevo trabajador
+    const valSchema = Yup.object().shape({
+        name: Yup.string()
+        .min(3,"El nombre del trabajador debe tener más de 3 caracteres")
+        .required("Por favor, ingresa el nombre del trabajador"),
+
+        lastName: Yup.string()
+        .min(3,"El apellido del trabajador debe tener más de 3 caracteres")
+        .required("Por favor, ingresar el apellido del trabajador"),
+
+        //queda pendiente validación de formato de rut
+        rut: Yup.string()
+        .required("Por favor, ingresar el rut del trabajador")
+
+
+    })
     return (
         <div>
-            <h1>"Nuevo Trabajador"</h1>
+            <h1 className='text-2xl text-center mt-8'>Formulario de nuevo trabajador</h1>
+
+            <div className='divide-y divide-slate-300/70'>
+
+                <div className='mt-9'>
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            lastName:'',
+                            rut:''
+                        }}
+
+                        validationSchema={valSchema}
+                        //agregar servicio de agregar trabajador
+                        onSubmit={values=>console.log(values)}
+                        enableReinitialize
+                    >
+                    {({errors,touched})=>(
+                        <Form className='text-center'>
+                            <div className='flex justify-center'>
+                                <div>
+                                    <div>
+                                        <label htmlFor='name'>Nombre:</label>
+                                        <Field id='name' type='text' name='name' className='bg-slate-200 mx-2 w-64 rounded-lg border border-stone-400'/>
+                                        {errors.name && touched.name ? <p className='text-red-600'>{errors.name}</p>: null}
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor='lastName'>Apellido:</label>
+                                        <Field id='lastName' type='text' name='lastName' className='bg-slate-200 mx-2 w-64 rounded-lg border border-stone-400 mt-5'/>
+                                        {errors.lastName && touched.lastName ? <p className='text-red-600'>{errors.lastName}</p>: null}
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor='rut'>Rut:</label>
+                                        <Field id='rut' type='text' name='rut' className='bg-slate-200 ml-9 w-64 rounded-lg border border-stone-400 mt-5'/>
+                                        {errors.rut && touched.rut ? <p className='text-red-600'>{errors.rut}</p>: null}
+                                    </div>
+                                </div>
+
+                                <div className='mt-5 ml-28 '>
+                                    <button className='bg-secondary-dark p-2.5 rounded-lg text-white text-xs mt-5' type='submit'>agregar</button>
+                                </div>
+                            </div>
+
+                        </Form>
+                    )}
+                    </Formik>
+                    
+                </div>
+
+                <div className='mt-9'>
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            lastName:'',
+                            rut:''
+                        }}
+
+                        validationSchema={valSchema}
+                        //agregar servicio de agregar trabajador
+                        onSubmit={values=>console.log(values)}
+                        enableReinitialize
+                    >
+                    {({errors,touched})=>(
+                        <Form className='text-center mt-8'>
+                            <div className='flex justify-center'>
+                                <div>
+                                    <div>
+                                        <label htmlFor='name'>Nombre:</label>
+                                        <Field id='name' type='text' name='name' className='bg-slate-200 mx-2 w-64 rounded-lg border border-stone-400'/>
+                                        {errors.name && touched.name ? <p className='text-red-600'>{errors.name}</p>: null}
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor='lastName'>Apellido:</label>
+                                        <Field id='lastName' type='text' name='lastName' className='bg-slate-200 mx-2 w-64 rounded-lg border border-stone-400 mt-5'/>
+                                        {errors.lastName && touched.lastName ? <p className='text-red-600'>{errors.lastName}</p>: null}
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor='rut'>Rut:</label>
+                                        <Field id='rut' type='text' name='rut' className='bg-slate-200 ml-9 w-64 rounded-lg border border-stone-400 mt-5'/>
+                                        {errors.rut && touched.rut ? <p className='text-red-600'>{errors.rut}</p>: null}
+                                    </div>
+                                </div>
+
+                                <div className='mt-5 ml-28 '>
+                                    <button className='bg-secondary-dark p-2.5 rounded-lg text-white text-xs mt-5' type='submit'>agregar</button>
+                                </div>
+                            </div>
+
+                        </Form>
+                    )}
+                    </Formik>
+                    
+                </div>
+
+                <div className='mt-9'>
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            lastName:'',
+                            rut:''
+                        }}
+
+                        validationSchema={valSchema}
+                        //agregar servicio de agregar trabajador
+                        onSubmit={values=>console.log(values)}
+                        enableReinitialize
+                    >
+                    {({errors,touched})=>(
+                        <Form className='text-center mt-8'>
+                            <div className='flex justify-center'>
+                                <div>
+                                    <div>
+                                        <label htmlFor='name'>Nombre:</label>
+                                        <Field id='name' type='text' name='name' className='bg-slate-200 mx-2 w-64 rounded-lg border border-stone-400'/>
+                                        {errors.name && touched.name ? <p className='text-red-600'>{errors.name}</p>: null}
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor='lastName'>Apellido:</label>
+                                        <Field id='lastName' type='text' name='lastName' className='bg-slate-200 mx-2 w-64 rounded-lg border border-stone-400 mt-5'/>
+                                        {errors.lastName && touched.lastName ? <p className='text-red-600'>{errors.lastName}</p>: null}
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor='rut'>Rut:</label>
+                                        <Field id='rut' type='text' name='rut' className='bg-slate-200 ml-9 w-64 rounded-lg border border-stone-400 mt-5'/>
+                                        {errors.rut && touched.rut ? <p className='text-red-600'>{errors.rut}</p>: null}
+                                    </div>
+                                </div>
+
+                                <div className='mt-5 ml-28 '>
+                                    <button className='bg-secondary-dark p-2.5 rounded-lg text-white text-xs mt-5' type='submit'>agregar</button>
+                                </div>
+                            </div>
+
+                        </Form>
+                    )}
+                    </Formik>
+                    
+                </div>
+
+
+            </div>
+
+            
+            
         </div>
     );
 }
