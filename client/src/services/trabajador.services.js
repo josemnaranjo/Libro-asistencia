d9f339115d894718b345f57c0d02ac04d1f43421
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-17 16:24:17 -0300

    Diseño basico front de view  nuevo dia

--- client/src/services/trabajador.services.js
+++ client/src/services/trabajador.services.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
 export const addTrabajador = async(values) => await axios.post('http://localhost:8000/api/crear-trabajador',values);
+export const getAllTrabajadores = async() => await axios.get('http://localhost:8000/api/get-all-trabajadores');
--- client/src/views/NuevoDia.jsx
+++ client/src/views/NuevoDia.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import {React, useState,useEffect}  from 'react';
+import { getAllTrabajadores } from '../services/trabajador.services';
+import {Formik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+
+
+
 
 const NuevoDia = () => {
+    const [trabajadores, setTrabajadores] = useState([]);
+
+    const getAllTrabajadoresFromService =  async() => {
+        const resutl = await getAllTrabajadores();
+        setTrabajadores(resutl.data);        
+    };
+
+    useEffect(() => {
+        getAllTrabajadoresFromService()
+    }, []);
     return (
         <div>
-            <h1>Nuevo día</h1>
+            <div className='mt-9'>
+                <Formik
+                    initialValues={{
+                        date:'',
+                        checked: []
+                    }}
+                    onSubmit={(values)=>{console.log(values)}}
+                >
+                    <Form >
+                        <div className='text-center'>
+                            <label htmlFor='date'>Fecha:</label>
+                            <Field id='date' type='text' name='date' className='text-xs bg-slate-200 mx-2 w-64 p-1 rounded-lg border border-stone-400'/>
+                        </div>
+                        <ul className='mt-10'>
+                            {trabajadores?.map((trabajador,i)=>(
+                                <li className='grid grid-cols-3 pl-48 mt-5' key={trabajador.id}>
+                                    <p className=''>{trabajador.name +" "+ trabajador.lastName}</p>
+                                    <p className=''>Rut: {trabajador.rut}</p>
+                                    <Field type="checkbox" name="checked" value={trabajador.id} className='ml-32'/>
+                                </li>
+                            ))}
+                        </ul>
+
+                        <div className='mt-9 text-center'>
+                                <button className='bg-secondary-dark p-2.5 rounded-lg text-white text-xs mt-5' type='submit'>
+                                    crear
+                                </button>
+                        </div>
+
+                    </Form>
+                </Formik>
+            </div>
         </div>
     );
 }
