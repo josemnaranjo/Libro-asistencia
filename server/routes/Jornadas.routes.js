1525d6c6c136f2411fef0a8519721a437d1cf637
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-20 10:48:57 -0300

    ruta add jornada

--- server/routes/Jornadas.routes.js
+++ server/routes/Jornadas.routes.js
@@ -1,1 +1,9 @@
 import { Router } from "express";
+import { addJornada } from '../controllers/jornada.controller.js';
+
+const router = Router();
+
+
+router.post('/api/crear-jornada',addJornada);
+
+export default router
