2e2a682b370740654827790b731d00242762c035
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-21 11:26:05 -0300

    modificaciones front nuevo dia

--- client/src/views/NuevoDia.jsx
+++ client/src/views/NuevoDia.jsx
@@ -1,14 +1,18 @@
 import {React, useState,useEffect}  from 'react';
 import { getAllTrabajadores } from '../services/trabajador.services';
 import {Formik, Form, Field} from 'formik';
-import * as Yup from 'yup';
 
 
 
 
 const NuevoDia = () => {
     const [trabajadores, setTrabajadores] = useState([]);
+    const newJornadaFromService = (values)=>{
+        const objPrincipal = {};
+        const arrPrincipal = [];
 
+    }
+  
     const getAllTrabajadoresFromService =  async() => {
         const resutl = await getAllTrabajadores();
         setTrabajadores(resutl.data);        
@@ -31,14 +35,14 @@ const NuevoDia = () => {
                     <Form >
                         <div className='text-center'>
                             <label htmlFor='date'>Fecha:</label>
-                            <Field id='date' type='text' name='date' className='text-xs bg-slate-200 mx-2 w-64 p-1 rounded-lg border border-stone-400'/>
+                            <Field placeholder="YYYY-MM-dd" id='date' type='text' name='date' className='text-xs bg-slate-200 mx-2 w-64 p-1 rounded-lg border border-stone-400'/>
                         </div>
                         <ul className='mt-10'>
                             {trabajadores?.map((trabajador,i)=>(
                                 <li className='grid grid-cols-3 pl-48 mt-5' key={trabajador.id}>
                                     <p>{trabajador.name +" "+ trabajador.lastName}</p>
                                     <p className='text-center'>Rut: {trabajador.rut}</p>
-                                    <Field type="checkbox" name="checked" value={trabajador.id.toString()} className='ml-32'/>
+                                    <Field type="checkbox" name="trabajadorId" value={trabajador.id.toString()} className='ml-32'/>
                                 </li>
                             ))}
                         </ul>
