3c1d07123042a48e3612d80da962c40633361379
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-23 12:52:55 -0300

    diseño de navbarsup

--- client/src/components/NavBarSup.jsx
+++ client/src/components/NavBarSup.jsx
@@ -9,16 +9,16 @@ const NavBarSup = () => {
 
     return (
         <div>
-            <nav className='bg-secondary-light h-32 flex'>
+            <nav className='bg-secondary-light h-32 grid grid-cols-3'>
                 <div>
-                    <Link to={`/libro-de-asistencias/${hoy}`} className="text-white">
-                        Hoy
+                    <Link to={`/libro-de-asistencias/${hoy}`}>
+                        <p className='text-white text-center h-full pt-12 border-r-2 border-blue-900 hover:bg-secondary-dark active:bg-secondary-middle'>Hoy</p>
                     </Link>
                 </div>
 
                 <div>
-                    <Link to={`/libro-de-asistencias/year/${year}`} className="text-white">
-                        {year}
+                    <Link to={`/libro-de-asistencias/year/${year}`}>
+                        <p className='text-white text-center h-full pt-12 border-r-2 border-blue-900 hover:bg-secondary-dark active:bg-secondary-middle'>{year}</p>
                     </Link>
                 </div>
             </nav>
