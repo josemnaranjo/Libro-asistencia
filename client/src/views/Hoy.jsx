7580dbb342c1ed9dbdcb0bad5c68241085931ffa
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-27 12:45:56 -0300

    Revert "cambios pequeños"
    This reverts commit cac91c6379a5d1fffbe4f6bd28ecdd3a614ff414.

--- client/src/views/Hoy.jsx
+++ client/src/views/Hoy.jsx
@@ -36,7 +36,6 @@ const Hoy = () => {
     return (
         <div>
             <h1 className='text-center text-lg pt-3'>{date}</h1>
-
             <div className='mt-9'>
                 <Formik>
                     <Form>
