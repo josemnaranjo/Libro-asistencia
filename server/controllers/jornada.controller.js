d2e9b447ab21dc93febda8ff10687b914f593edd
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-28 15:25:25 -0300

    modificacion  updateHorasJornada controller para que maneje distintos casos de envio de datos

--- server/controllers/jornada.controller.js
+++ server/controllers/jornada.controller.js
@@ -24,13 +24,44 @@ export const addJornada = async(req,res)=> {
 export const updateHorasEnJornada = async(req,res)=>{
     try{
         const {horaInicio, horaTermino, date, trabajadorId} =req.body;
-        await Jornada.update({horaInicio:horaInicio,horaTermino:horaTermino},{
-            where:{
-                trabajadorId:trabajadorId,
-                date:date
-            }
-        });
-        res.json({"Jornada actualizada con éxito. TrabajadorID:": trabajadorId});
+
+
+
+        if(horaInicio ===''&& horaTermino===''){
+            res.json("Debes ingresar un valor en alguno de las horas");
+        }
+
+        else if(horaInicio === ''){
+            await Jornada.update({horaTermino:horaTermino},{
+                where:{
+                    trabajadorId:trabajadorId,
+                    date:date
+                }
+            });
+
+            res.json({"Jornada actualizada con éxito. TrabajadorID:": trabajadorId});
+
+        }
+        else if(horaTermino === ''){
+            await Jornada.update({horaInicio:horaInicio},{
+                where:{
+                    trabajadorId:trabajadorId,
+                    date:date
+                }
+            });
+
+            res.json({"Jornada actualizada con éxito. TrabajadorID:": trabajadorId});
+        }
+        else{
+            await Jornada.update({horaInicio:horaInicio,horaTermino:horaTermino},{
+                where:{
+                    trabajadorId:trabajadorId,
+                    date:date
+                }
+            });
+            res.json({"Jornada actualizada con éxito. TrabajadorID:": trabajadorId});
+        }
+        
 
     }catch{
         res.status(500).json({error:"Algo salió mal al crear la nueva jornada",err})
