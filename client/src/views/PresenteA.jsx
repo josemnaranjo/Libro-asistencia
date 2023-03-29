2e556558de83729e0403673ce48251a82b4281c6
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-03-29 17:28:52 -0300

    diseño basico view presenta año

--- client/src/views/PresenteA.jsx
+++ client/src/views/PresenteA.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
 const PresenteA = () => {
+
     return (
+
         <div>
-            <h1>Presente año</h1>
+            <div className='grid grid-rows-4 gap-y-14 mt-20'>
+
+                <div className='grid grid-cols-3 justify-items-center'>
+                    <h1>Enero</h1>
+                    <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'> informe de asistencias</button>
+                </div>
+
+                <div className='grid grid-cols-3 justify-items-center'>
+                    <h1>Febrero</h1>
+                    <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'> informe de asistencias</button>
+                </div>
+
+                <div className='grid grid-cols-3 justify-items-center '>
+                    <h1>Marzo</h1>
+                    <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'> informe de asistencias</button>
+                </div>
+
+                <div className='grid grid-cols-3 justify-items-center'>
+                    <h1>Abril</h1>
+                    <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'> informe de asistencias</button>
+                </div>
+
+            </div>
+
         </div>
     );
 }
