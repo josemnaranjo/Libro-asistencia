4cd94c7ae41b5b33d47ca02cad6e8057693a4061
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-03 10:39:39 -0400

    diseño responsivo barra superior de navegacion

--- client/src/components/NavBarSup.jsx
+++ client/src/components/NavBarSup.jsx
@@ -10,16 +10,16 @@ const NavBarSup = () => {
 
     return (
         <div>
-            <nav className='bg-secondary-light h-20 grid grid-cols-3'>
+            <nav className='bg-secondary-light h-1/6 grid grid-cols-3 content-center'>
                 <div>
                     <Link to={`/libro-de-asistencias/${hoy}`}>
-                        <p className='text-white text-center h-full pt-8 border-r-2 border-blue-600 hover:bg-secondary-dark active:bg-secondary-middle'>Hoy</p>
+                        <p className='text-white text-center pt-5 border-r-2 border-blue-600 hover:bg-secondary-dark active:bg-secondary-middle'>Hoy</p>
                     </Link>
                 </div>
 
                 <div>
                     <Link to={`/libro-de-asistencias/year/${year}`}>
-                        <p className='text-white text-center h-full pt-8 border-r-2 border-blue-600 hover:bg-secondary-dark active:bg-secondary-middle'>{year}</p>
+                        <p className='text-white text-center pt-5 border-r-2 border-blue-600 hover:bg-secondary-dark active:bg-secondary-middle'>{year}</p>
                     </Link>
                 </div>
             </nav>
