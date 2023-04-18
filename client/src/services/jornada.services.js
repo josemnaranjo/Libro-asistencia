0cd58b820ab7c285399399e649cea1a2264320c8
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-18 11:03:27 -0400

    feat!: servicio de registro de salida

--- client/src/services/jornada.services.js
+++ client/src/services/jornada.services.js
@@ -4,5 +4,5 @@ import axios from 'axios';
 export const registroDeEntrada = async(date,rut) => axios.post(`http://localhost:8000/api/registro-de-entrada/${date}`,rut);
 
 
-export const updateHorasJornada = async(values) => axios.put('http://localhost:8000/api/actualizar-hora-jornada',values);
+export const registroDeSalida= async(date,rut) => axios.put(`http://localhost:8000/api/registro-de-salida/${date}`,rut);
 
