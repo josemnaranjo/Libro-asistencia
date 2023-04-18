c91f137318cf0fc8f3d09d11cb3d1ccc458fa88f
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-18 10:26:30 -0400

    feat!:service para crear nueva jornada ingresando rut de trabajador

--- client/src/services/jornada.services.js
+++ client/src/services/jornada.services.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-export const createNewJornada = async(values) => axios.post('http://localhost:8000/api/crear-jornada',values);
+export const registroDeEntrada = async(date,rut) => axios.post(`http://localhost:8000/api/registro-de-entrada/${date}`,rut);
 
 
-export const updateHorasJornada = async(values) => axios.put('http://localhost:8000/api/actualizar-hora-jornada',values)
+export const updateHorasJornada = async(values) => axios.put('http://localhost:8000/api/actualizar-hora-jornada',values);
+
