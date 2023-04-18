30fa7c56bcfdaee761001376ae02c86ca659f0ea
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-18 15:58:01 -0400

    style: ingreso de fechas con datepicker

--- client/src/views/NuevoDia.jsx
+++ client/src/views/NuevoDia.jsx
@@ -1,35 +1,39 @@
-import {React}  from 'react';
-import {useNavigate} from 'react-router-dom';
-import {Formik,Form,Field} from 'formik';
+import {React, useState,useEffect}  from 'react';
+import {Link} from 'react-router-dom';
 import dayjs from 'dayjs';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
 
 const NuevoDia = () => {
-    const navigate = useNavigate();
+    const [selectedDay, setSelectedDay] = useState(null);
+    const [formatedDay , setFormatedDay] = useState();
+    
+    const changeDateFormat = (data) => {
+        const newDate = dayjs(data).format('YYYY-M-D');
+        setFormatedDay(newDate);
+    };
+
+
     return (
         <div className='pt-12 px-6 h-5/6'>
-            <div className='px-10 py-10 h-5/6 bg-gradient-to-r from-slate-100 to-slate-300 border-xl rounded-xl'>
+            <div className='px-10 py-40 h-5/6 bg-gradient-to-r from-slate-100 to-slate-300 border-xl rounded-xl'>
                 <h1 className='text-center py-10'>Ingrese la fecha de hoy para iniciar un nuevo registro</h1>
-                <Formik
-                    initialValues={{
-                        date:''
-                    }}
-                    onSubmit={(values)=>{
-                        const formatedDate = dayjs(values.date);
-                        console.log(formatedDate);
-                        // navigate(`/registro-entrada/${formatedDate}`);
-                    }}
-                >
-                    <Form className='text-center py-24'>
-                        <div>
-                            <label htmlFor="date">Fecha:</label>
-                            <Field id='date' type='text' name='date' className='text-xs bg-white w-28 py-1 px-2 rounded-lg border border-stone-400'/>
-                        </div>
-
-                        <div className='py-9'>
-                            <button className='bg-secondary-dark p-2.5 rounded-lg text-white text-xs' type='submit'>crear nuevo registro</button>
-                        </div>
-                    </Form>
-                </Formik>
+                <div className='grid grid-rows-2 justify-items-center gap-5'>
+                    <div>
+                        <DatePicker
+                            selected={selectedDay}
+                            onChange={date => setSelectedDay(date)}
+                            dateFormat={'dd-MM-yyyy'}
+                            minDate={new Date()}
+                            placeholderText='Elegir una fecha'
+                            className='text-center text-sm bg-white w-48 py-1 px-2 rounded-lg border border-stone-400'
+                        />
+                    </div>
+                    <Link to={`/registro-entrada/${formatedDay}`}>
+                        <button className='bg-secondary-dark p-1.5 rounded-lg text-white'onMouseEnter={()=>changeDateFormat(selectedDay)} >crear nuevo día</button>
+                    </Link>
+                </div>
+                
             </div>
         </div>
     );
