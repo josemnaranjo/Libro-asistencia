681ff3e091b433d49cd3e2b2fda2af4161fde059
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-18 16:03:00 -0400

    style:nuevo diseño para field

--- client/src/views/RegistroEntrada.jsx
+++ client/src/views/RegistroEntrada.jsx
@@ -31,7 +31,7 @@ const RegistroDeEntrada = () => {
                     <Form className='py-12'>
                         <div className='grid grid-rows-2 justify-items-center gap-3'>
                             <label htmlFor="rut">Ingrese su rut</label>
-                            <Field id='rut' type='text' name='rut' placeholder="XX.XXX.XXX-X" className='text-xs bg-white w-28 py-1 px-2 rounded-lg border border-stone-400'/>
+                            <Field id='rut' type='text' name='rut' placeholder="XX.XXX.XXX-X" className=' text-center text-sm bg-white w-38 py-1 px-2 rounded-lg border border-stone-400'/>
                         </div>
 
                         <div className='text-center py-9'>
