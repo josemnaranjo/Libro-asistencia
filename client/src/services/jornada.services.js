e7ff0086cebb079137abe9bbca9838d52a743483
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-19 16:04:55 -0400

    feat!: registro de ausentes

--- client/src/services/jornada.services.js
+++ client/src/services/jornada.services.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const registroDeEntrada = async(date,rut) => axios.post(`http://localhost:8000/api/registro-de-entrada/${date}`,rut);
 
 
-export const registroDeSalida= async(date,rut) => axios.put(`http://localhost:8000/api/registro-de-salida/${date}`,rut);
+export const registroDeSalida = async(date,rut) => axios.put(`http://localhost:8000/api/registro-de-salida/${date}`,rut);
 
-
+export const registroDeAusentes = async(date) => axios.put(`http://localhost:8000/api/registro-ausencia/${date}`);
 
