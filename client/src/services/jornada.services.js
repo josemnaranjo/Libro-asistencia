aff125b904aaae8667d333a8fd6b63c0a7c6f127
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-19 15:59:34 -0400

    feat: ingreso de ausentes

--- client/src/services/jornada.services.js
+++ client/src/services/jornada.services.js
@@ -6,3 +6,5 @@ export const registroDeEntrada = async(date,rut) => axios.post(`http://localhost
 
 export const registroDeSalida= async(date,rut) => axios.put(`http://localhost:8000/api/registro-de-salida/${date}`,rut);
 
+
+
--- client/src/views/RegistroAusentes.jsx
+++ client/src/views/RegistroAusentes.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import dayjs from 'dayjs';
+import {getAllTrabajadoresOfAJornada} from '../services/trabajador.services.js';
 
 const RegistroAusentes = () => {
+
+    const {date} = useParams();
+    const dateFormated = dayjs(date).format('D-M-YYYY');
+    const [trabajadorData,setTrabajadorData] = useState();
+
+    const getAllTrabajadoresOfAJornadaFromService = async()=>{
+        try{
+            const information = await getAllTrabajadoresOfAJornada(date);
+            setTrabajadorData(information.data.jornadaInfo);
+            console.log(information.data.jornadaInfo);
+        }catch(err){
+            console.log(err)
+        }
+    };
+
+    useEffect(() => {
+        getAllTrabajadoresOfAJornadaFromService();
+    }, []);
+
+
     return (
         <div className='pt-12 px-6 h-5/6'>
             <div className='px-10 py-10 h-5/6 bg-gradient-to-r from-slate-100 to-slate-300 border-xl rounded-xl'>
-                <h1>Registro de ausentes</h1>
+                <h1 className='text-center'>Registro de ausentes : {dateFormated}</h1>
+                <ul className='py-5'>
+                    {
+                        trabajadorData?.map((t)=>(
+                            <li key={t.id} className='grid grid-cols-5 py-2 justify-items-center'>
+                                <p>
+                                    Nombre: {t.Trabajador.name} {t.Trabajador.lastName}
+                                </p>
+                                <p>
+                                    Rut: {t.Trabajador.rut}
+                                </p>
+                                <p>
+                                    Hora Inicio: {t.horaInicio}
+                                </p>
+                                <p>
+                                    Hora Termino: {t.horaTermino}
+                                </p>
+                                <button className='bg-primary-dark py-1 w-24 rounded-lg text-white'>
+                                    ausente
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </div>
     );
