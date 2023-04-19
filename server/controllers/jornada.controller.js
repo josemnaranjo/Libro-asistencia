91faeee757ee51369927d15bed33d271415a6e44
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-19 15:12:13 -0400

    feat!: funcionalidad de registro de ausentes

--- server/controllers/jornada.controller.js
+++ server/controllers/jornada.controller.js
@@ -54,9 +54,32 @@ export const registroDeSalida = async(req,res)=> {
 
         await Jornada.update({horaTermino},{where:{date:date,trabajadorId:trabajadorId}});
 
-        res.json({message:"Registro de salida creado exitosamente" })
+        res.json({message:"Registro de salida creado exitosamente"})
 
     }catch(err){
         res.status(500).json({error:"Algo salió mal al crear la nueva jornada",err})
     }
 };
+
+export const registroDeAusente = async(req,res)=>{
+    try{
+        const {date} = req.params;
+        const {rut} = req.body;
+
+
+        const trabajador = await Trabajador.findAll({
+            where:{
+                rut:rut
+            }
+        });
+        
+        const trabajadorId = trabajador[0].id;
+
+        await Jornada.update({ausente: true}, {where:{date:date,trabajadorId:trabajadorId}});
+
+        res.json({message:"Registro de ausencia actualizado exitosamente"})
+
+    }catch(err){
+        res.status(500).json({error:"Algo salió mal al registrar la ausencia del trabajador",err})
+    }
+}
--- server/routes/Jornadas.routes.js
+++ server/routes/Jornadas.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registroDeEntrada , registroDeSalida  } from '../controllers/jornada.controller.js';
+import { registroDeEntrada , registroDeSalida, registroDeAusente  } from '../controllers/jornada.controller.js';
 
 const router = Router();
 
 
 router.post('/api/registro-de-entrada/:date',registroDeEntrada );
 router.put('/api/registro-de-salida/:date',registroDeSalida);
+router.put('/api/registro-ausencia/:date',registroDeAusente);
+
 
 
 export default router
