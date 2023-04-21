372d91794e3b9636d4dedde953c2a854c6dcf5d8
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-04-21 10:13:57 -0400

    style: cambio texto boton a descargar informe

--- client/src/components/PresenteAUno.jsx
+++ client/src/components/PresenteAUno.jsx
@@ -11,25 +11,25 @@ const PresenteAUno = (props) => {
                 <div className='grid grid-cols-3 justify-items-center'>
                     <h1>Enero</h1>
                     <button className='bg-secondary-dark rounded-lg text-white w-32 h-10' >ver detalles</button>
-                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3' onClick={()=>onSubmitProps({dateStart:`${fechaAno}-01-01`, mes:"enero", dateFinish:`${fechaAno}-01-31`})} value="download"> informe de asistencias</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3' onClick={()=>onSubmitProps({dateStart:`${fechaAno}-01-01`, mes:"enero", dateFinish:`${fechaAno}-01-31`})} value="download"> descargar informe </button>
                 </div>
 
                 <div className='grid grid-cols-3 justify-items-center'>
                     <h1>Febrero</h1>
                     <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
-                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'onClick={()=>onSubmitProps({dateStart:`${fechaAno}-02-01`, mes:"febrero", dateFinish:`${fechaAno}-02-28`})}> informe de asistencias</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'onClick={()=>onSubmitProps({dateStart:`${fechaAno}-02-01`, mes:"febrero", dateFinish:`${fechaAno}-02-28`})}> descargar informe</button>
                 </div>
 
                 <div className='grid grid-cols-3 justify-items-center '>
                     <h1>Marzo</h1>
                     <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
-                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3' onClick={()=>onSubmitProps({dateStart:`${fechaAno}-03-01`, mes:"marzo", dateFinish:`${fechaAno}-03-31`})}> informe de asistencias</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3' onClick={()=>onSubmitProps({dateStart:`${fechaAno}-03-01`, mes:"marzo", dateFinish:`${fechaAno}-03-31`})}> descargar informe</button>
                 </div>
 
                 <div className='grid grid-cols-3 justify-items-center'>
                     <h1>Abril</h1>
                     <button className='bg-secondary-dark rounded-lg text-white w-32 h-10'>ver detalles</button>
-                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'onClick={()=>onSubmitProps({dateStart:`${fechaAno}-04-01`, mes:"abril", dateFinish:`${fechaAno}-04-30`})}> informe de asistencias</button>
+                    <button className='bg-secondary-dark rounded-lg text-white w-38 h-10 px-3'onClick={()=>onSubmitProps({dateStart:`${fechaAno}-04-01`, mes:"abril", dateFinish:`${fechaAno}-04-30`})}> descargar informe</button>
                 </div>
 
             </div>
