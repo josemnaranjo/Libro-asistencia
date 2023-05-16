cf945059079bb36cbeb7726f1ec5b1f98da3d178
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-05-16 12:11:57 -0400

    chore: se creo componente formulario de registro

--- client/src/views/RegistroEntrada.jsx
+++ client/src/views/RegistroEntrada.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { registroDeEntrada } from "../services/jornada.services.js";
-import { Formik, Form, Field } from "formik";
+import FormularioRegistro from "../components/FormularioRegistro.jsx";
 import { useParams } from "react-router-dom";
 import dayjs from "dayjs";
 import Swal from "sweetalert2";
@@ -31,44 +31,13 @@ const RegistroDeEntrada = () => {
       console.log(err);
     }
   };
+
   return (
     <div className="h-5/6 flex justify-center items-center px-12">
       <div className="border-xl h-fit w-full rounded-xl bg-gradient-to-r from-slate-100 to-slate-300 px-10 py-28">
         <h1 className="py-2 text-center text-2xl">Registro de entrada</h1>
         <h1 className="py-2 text-center text-lg">{dateForDisplay}</h1>
-        <Formik
-          initialValues={{
-            rut: "",
-          }}
-          onSubmit={(values, { resetForm }) => {
-            registroDeEntradaFromService(values);
-            resetForm();
-          }}
-        >
-          <Form className="py-12">
-            <div className="grid grid-rows-2 justify-items-center gap-3">
-              <label htmlFor="rut" className="text-lg">
-                Ingrese su rut
-              </label>
-              <Field
-                id="rut"
-                type="text"
-                name="rut"
-                placeholder="XX.XXX.XXX-X"
-                className=" w-38 rounded-lg border border-stone-400 bg-white px-2 py-1 text-center text-sm"
-              />
-            </div>
-
-            <div className="py-9 text-center">
-              <button
-                className="rounded-lg bg-secondary-dark p-2.5 text-sm text-white"
-                type="submit"
-              >
-                aceptar
-              </button>
-            </div>
-          </Form>
-        </Formik>
+        <FormularioRegistro onSubmitProps={registroDeEntradaFromService}/>
       </div>
     </div>
   );
--- client/src/views/RegistroSalida.jsx
+++ client/src/views/RegistroSalida.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { registroDeSalida } from "../services/jornada.services.js";
-import { Formik, Form, Field } from "formik";
+import FormularioRegistro from "../components/FormularioRegistro.jsx";
 import { useParams } from "react-router-dom";
 import dayjs from "dayjs";
 import Swal from "sweetalert2";
@@ -8,6 +8,7 @@ import Swal from "sweetalert2";
 const RegistroDeSalida = () => {
   const { date } = useParams();
   const dateForDisplay = dayjs(date).format("D-M-YYYY");
+
   const registroDeSalidaFromService = async (rut) => {
     try {
       await registroDeSalida(date, rut);
@@ -36,39 +37,7 @@ const RegistroDeSalida = () => {
       <div className="border-xl h-fit w-full rounded-xl bg-gradient-to-r from-slate-100 to-slate-300 px-10 py-28">
         <h1 className="py-2 text-center text-2xl">Registro de salida</h1>
         <h1 className="py-2 text-center text-lg">{dateForDisplay}</h1>
-        <Formik
-          initialValues={{
-            rut: "",
-          }}
-          onSubmit={(values, { resetForm }) => {
-            registroDeSalidaFromService(values);
-            resetForm();
-          }}
-        >
-          <Form className="py-12">
-            <div className="grid grid-rows-2 justify-items-center gap-3">
-              <label htmlFor="rut" className="text-lg">
-                Ingrese su rut
-              </label>
-              <Field
-                id="rut"
-                type="text"
-                name="rut"
-                placeholder="XX.XXX.XXX-X"
-                className=" w-38 rounded-lg border border-stone-400 bg-white px-2 py-1 text-center text-sm"
-              />
-            </div>
-
-            <div className="py-9 text-center">
-              <button
-                className="rounded-lg bg-secondary-dark p-2.5 text-xs text-white"
-                type="submit"
-              >
-                aceptar
-              </button>
-            </div>
-          </Form>
-        </Formik>
+        <FormularioRegistro onSubmitProps={registroDeSalidaFromService}/>
       </div>
     </div>
   );
